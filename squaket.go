4716c35f3ec785333be25db1a1bca977dcd87c9e
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-09-28 10:09:37 +0100

    Progress

---
+++ squaket.go
@@ -0,0 +1,35 @@
+package squaket
+
+import (
+	"errors"
+	"reflect"
+)
+
+func New (element []interface{}) (*Squaket, error) {
+	for _, someElement := range element {
+		if reflect.ValueOf (someElement).Kind () != reflect.Struct {
+			return nil, errors.New ("An element is not a struct.")
+		}
+	}
+	return &Squaket {element}, nil
+}
+
+type Squaket struct {
+	element []interface {}
+}
+
+func (s *Squaket) Group (property string) (g map[interface {}][]interface {}, e error) {
+	for _, element := range s.element {
+		value := reflect.ValueOf (element).FieldByName (property)
+		if value.IsValid () == false {
+			return nil, errors.New ("An element does not have that field.")
+		}
+		elements, okX := g [value.Interface ()]
+		if okX == false {
+			elements = []interface {} {}
+		}
+		elements = append (elements, element)
+		g [value.Interface ()] = elements
+	}
+	return
+}
